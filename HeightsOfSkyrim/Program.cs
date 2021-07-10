dd26afd0a4799006118b3d63433293e2b866efa9
Бугаев Андрей <bugaev.andrei55@gmail.com>
2021-07-11 00:47:55 +0600

    Initial commit

---
+++ HeightsOfSkyrim/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+
+namespace HeightsOfSkyrim
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "HeightOfSkyrimPatch.esp")
+                .AddRunnabilityCheck(state => state.LoadOrder.AssertHasMod(ModKey.FromNameAndExtension("Heights_of_Skyrim.esp"), true, "\n\nYour Heights_of_Skyrim.esp is not in load order or above Synthesis.esp in LO\n\n"))
+                .Run(args);
+        }
+
+        private static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            ModKey heights = ModKey.FromNameAndExtension("Heights_of_Skyrim.esp");
+            state.LoadOrder.TryGetIfEnabledAndExists(heights, out ISkyrimModGetter heightsMod);
+            if (heightsMod is null)
+            {
+                throw new Exception("Your Heights_of_Skyrim.esp is not in load order or above Synthesis.esp in LO");
+            }
+            foreach (INpcGetter npc in heightsMod.Npcs)
+            {
+                INpcGetter winningOverride = npc.AsLink().Resolve(state.LinkCache);
+                if (Math.Abs(winningOverride.Height - npc.Height) < 0.00001) continue;
+                INpc patchedNpc = state.PatchMod.Npcs.GetOrAddAsOverride(winningOverride);
+                patchedNpc.Height = npc.Height;
+            }
+        }
+    }
+}
